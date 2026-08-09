989ab42530c17d26419b68a623120224801ec82b
pshts <pshts@PSHT>
2026-08-09 10:44:29 +0200

    Projektdateien hinzufügen.

---
+++ Cashbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cashbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
